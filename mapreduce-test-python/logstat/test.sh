8aa74a405920335d120a96fbbcef5a5d03082733
Luis San Lucas <luisesanlucas@gmail.com>
2021-03-15 13:41:48 -0400

    fixed bugs

--- mapreduce-test-python/logstat/test.sh
+++ mapreduce-test-python/logstat/test.sh
@@ -6,7 +6,7 @@
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../../mapreduce-test-python/logstat/mapper.py -mapper ../../mapreduce-test-python/logstat/mapper.py \
--file ../../mapreduce-test-python/logstat/reducer.py -reducer '../../mapreduce-test-python/logstat/reducer.py 14 17 \'
+-file ../../mapreduce-test-python/logstat/reducer.py -reducer '../../mapreduce-test-python/logstat/reducer.py 14 17'
 -input /logstat/input/* -output /logstat/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat/input/
