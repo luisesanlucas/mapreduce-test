dfe0acd6a4bd2f153bd66f809b300253c2c475d2
Luis San Lucas <luisesanlucas@gmail.com>
2021-03-15 13:50:58 -0400

    Fixed args bug

--- mapreduce-test-python/logstat/reducer.py
+++ mapreduce-test-python/logstat/reducer.py
@@ -1,47 +1,46 @@
 #!/usr/bin/python
 
-if __name__ == "__main__":
-    from operator import itemgetter
-    import sys
-    from collections import defaultdict
+from operator import itemgetter
+import sys
+from collections import defaultdict
 
-    dict_ip_count = {}
+dict_ip_count = {}
 
-    for line in sys.stdin:
-        line = line.strip()
-        ip, num = line.split('\t')
-        try:
-            num = int(num)
-            dict_ip_count[ip] = dict_ip_count.get(ip,0) + num
+for line in sys.stdin:
+    line = line.strip()
+    ip, num = line.split('\t')
+    try:
+        num = int(num)
+        dict_ip_count[ip] = dict_ip_count.get(ip,0) + num
 
-        except ValueError:
-            pass
+    except ValueError:
+        pass
 
 
-    sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
+sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
 
-    freq_ips = defaultdict(list)
+freq_ips = defaultdict(list)
 
-    for ip, count in sorted_dict_ip_count:
-        h,ip_ = ip.split(' ')
+for ip, count in sorted_dict_ip_count:
+    h,ip_ = ip.split(' ')
 
-        try:
-            h = int(h)
-            count = int(count)
-            freq_ips[h].append([ip,count])
+    try:
+        h = int(h)
+        count = int(count)
+        freq_ips[h].append([ip,count])
 
-        except ValueError:
-            pass
-    '''
-    beg = input ('Enter start time: ')
-    beg = int(beg)
-    en = input ('Enter end time: ')
-    en = inst(en)
-    '''
+    except ValueError:
+        pass
+'''
+beg = input ('Enter start time: ')
+beg = int(beg)
+en = input ('Enter end time: ')
+en = inst(en)
+'''
 
-    beg = int(sys.argv[1])
-    en = int(sys.argv[2])
+beg = int(sys.argv[1])
+en = int(sys.argv[2])
 
-    for i in range(beg,en):
-        t_3 = sorted(freq_ips[i], key=lambda v:v[1], reverse=True)[0:3]
-        print '%s\t%s' % (i, t_3)
+for i in range(beg,en):
+    t_3 = sorted(freq_ips[i], key=lambda v:v[1], reverse=True)[0:3]
+    print '%s\t%s' % (i, t_3)
