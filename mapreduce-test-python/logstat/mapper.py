fd5b8fd4f17cb6facb0ae3a0f93e6ec30fea3a3b
Luis San Lucas <luisesanlucas@gmail.com>
2021-03-15 14:35:59 -0400

    bug

--- mapreduce-test-python/logstat/mapper.py
+++ mapreduce-test-python/logstat/mapper.py
@@ -10,5 +10,5 @@ pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     match = pat.search(line)
     if match:
-        if int(match.group('hour')>=beg and int(match.group('hour')<=en:
+        if int(match.group('hour') >= beg and int(match.group('hour') < en:
             print '%s\t%s' % (match.group('hour') + ' ' + match.group('ip'), 1)
